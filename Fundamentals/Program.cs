0d00c634b21c7ed5c4919cb83bf1005e68abf58b
Einar Ingebrigtsen <einar@dolittle.com>
2019-08-28 18:46:20 +0200

    Scaffolded

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Dolittle.Booting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder();
+            hostBuilder.ConfigureLogging(_ => {
+                _.AddConsole();
+            });
+            hostBuilder.UseEnvironment("Development");
+            var host = hostBuilder.Build();
+            var loggerFactory = host.Services.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
+
+            var result = Bootloader.Configure(_ => {
+                _.UseLoggerFactory(loggerFactory);
+                _.Development();
+            }).Start();
+            
+            await host.RunAsync();
+        }
+    }
+}
